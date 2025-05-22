c0b34bee666fbd22a7e59777499ddddb7f6b7eda
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-22 01:53:20 -0500

    Update TestBFS.java

--- avltree/TestBFS.java
+++ avltree/TestBFS.java
@@ -4,6 +4,24 @@ import Exceptions.ItemDuplicated;
 
 public class TestBFS {
     public static void main(String[] args) {
+        AVLTree<Integer> avl = new AVLTree<>();
 
+        try {
+            avl.insert(50);
+            avl.insert(30);
+            avl.insert(70);
+            avl.insert(20);
+            avl.insert(40);
+            avl.insert(80);
+            avl.insert(60);
+            avl.insert(10);
+            avl.insert(25);
+            avl.insert(65);
+        } catch (ItemDuplicated e) {
+            System.out.println("Error: elemento duplicado");
+        }
+
+        System.out.println("Recorrido BFS (por niveles):");
+        avl.bfs();
     }
 }
