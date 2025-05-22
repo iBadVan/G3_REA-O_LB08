06a26ba90f7b25d5870a05af8ca1ddd12b1497cb
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-22 00:54:22 -0500

    Update TestAVL.java

--- avltree/TestAVL.java
+++ avltree/TestAVL.java
@@ -3,6 +3,29 @@ import Exceptions.ItemDuplicated;
 
 public class TestAVL {
     public static void main(String[] args) {
+        AVLTree<Integer> avl = new AVLTree<>();
+
+        try {
+            avl.insert(30);
+            avl.insert(20);
+            avl.insert(10);  
+
+            avl.insert(40);
+            avl.insert(50);  
+
+            avl.insert(25);
+            avl.insert(22);
+
+            avl.insert(45);
+            avl.insert(47);  
+
+            avl.insert(60);
+            avl.insert(5);
+
+        } catch (ItemDuplicated e) {
+            System.out.println("Error: Elemento duplicado");
+        }
         
+        avl.inOrder();
     }
 }
