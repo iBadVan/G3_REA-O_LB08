d56d44ccb37430d899e9ad907f8da1bfaf228a30
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-22 00:14:46 -0500

    Update BinarySearchTree.java

--- avltree/BinarySearchTree.java
+++ avltree/BinarySearchTree.java
@@ -1,5 +1,12 @@
 package avltree;
 
-public class BinarySearchTree {
-    
+public interface BinarySearchTree<E extends Comparable<E>> {
+    void insert(E data) throws Exception;
+    void remove(E data) throws Exception;
+    boolean contains(E data);
+    E findMin() throws Exception;
+    E findMax() throws Exception;
+    boolean isEmpty();
+    void clear();
+    int size();
 }
