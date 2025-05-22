450598a0441387913bca0faeee0d62b4a062bff3
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-22 00:23:21 -0500

    Update AVLTree.java

--- avltree/AVLTree.java
+++ avltree/AVLTree.java
@@ -1,5 +1,13 @@
 package avltree;
 
 public class AVLTree<E extends Comparable<E>> extends BSTree<E> {
-    
+    class NodeAVL extends Node {
+        protected int bf;  
+
+        public NodeAVL(E data) {
+            super(data); 
+            this.bf = 0; 
+        }
+
+    }
 }
