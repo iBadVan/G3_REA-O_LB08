f93b8667a173a72b0ea9b43c522391b9a7a4dd0b
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-22 01:41:55 -0500

    Update TestAVLLevelOrder.java

--- avltree/TestAVLLevelOrder.java
+++ avltree/TestAVLLevelOrder.java
@@ -1,7 +1,25 @@
 package avltree;
 
+import Exceptions.ItemDuplicated;
+
 public class TestAVLLevelOrder {
     public static void main(String[] args) {
         AVLTree<Integer> avl = new AVLTree<>();
+        try {
+            avl.insert(50);
+            avl.insert(30);
+            avl.insert(70);
+            avl.insert(30); 
+            avl.insert(40);
+            avl.insert(60);
+            avl.insert(80);
+            avl.insert(35);
+            avl.insert(40); 
+        } catch (ItemDuplicated e) {
+            System.out.println("Error: elemento duplicado");
+        }
+
+        System.out.println("Recorrido por amplitud (nivel por nivel):");
+        avl.levelOrder();
     }
 }
