973de4f68d000421be336d054bd8961141e80324
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-22 00:39:50 -0500

    Update AVLTree.java

--- avltree/AVLTree.java
+++ avltree/AVLTree.java
@@ -22,5 +22,38 @@ public class AVLTree<E extends Comparable<E>> extends BSTree<E> {
         this.height = false;
         this.root = insert(x, (NodeAVL) this.root);
     }
+    protected Node insert(E x, NodeAVL node) throws ItemDuplicated {
+        NodeAVL fat = node;
 
+        if (node == null) {
+            this.height = true;
+            fat = new NodeAVL(x);
+        } else {
+            int resC = node.data.compareTo(x);
+            if (resC == 0)
+                throw new ItemDuplicated(x + " ya se encuentra en el árbol...");
+
+            if (resC < 0) {  // Insertar en el subárbol derecho
+                fat.right = insert(x, (NodeAVL) node.right);
+                if (this.height) {
+                    switch (fat.bf) {
+                        case -1:
+                            fat.bf = 0;
+                            this.height = false;
+                            break;
+                        case 0:
+                            fat.bf = 1;
+                            this.height = true;
+                            break;
+                        case 1:
+                            fat = balanceToLeft(fat);
+                            this.height = false;
+                            break;
+                    }
+                }
+            } else {  
+            }
+        }
+        return fat;
+    }
 }
