e9f7a8fef95ae1ada17a7e4354bb4e55d11940c1
iBadVan <ivan.edgardo.reso@gmail.com>
2025-05-22 00:21:07 -0500

    Update BSTree.java

--- avltree/BSTree.java
+++ avltree/BSTree.java
@@ -1,5 +1,21 @@
 package avltree;
 
 public class BSTree<E extends Comparable<E>> implements BinarySearchTree<E> {
-    
+    protected class Node {
+        protected E data;
+        protected Node left;
+        protected Node right;
+
+        public Node(E data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    protected Node root;
+
+    public BSTree() {
+        this.root = null;
+    }
 }
